6841a40e708d1757a2863c3776e0d3a9e7dc590c
HongYang Gao <hongyang.gao@gmail.com>
2019-04-22 15:56:06 -0700

    Solve problem 1.2

--- CTCI_Problems/arrays&strings.js
+++ CTCI_Problems/arrays&strings.js
@@ -17,7 +17,38 @@ function isUnique (str) {
 // console.log(isUnique('abc'));
 // console.log(isUnique('aba'));
 
+
+
 // #1.2
 
+function isPermutation (string, substr) {
+    if (string.length !== substr.length) return false;
+    let hash = {};
+
+    for (let i = 0; i < string.length; i++) {
+        if (hash[string[i]] === undefined) {
+            hash[string[i]] = 1;
+        } else {
+            hash[string[i]] += 1;
+        }
+    }
+
+    console.log(hash)
+
+    for (let j = 0; j < substr.length; j++) {
+        if (hash[substr[j]] === undefined || hash[substr[j]] === 0) {
+            return false;
+        } else {
+            hash[substr[j]] -= 1;
+        }
+    }
+
+    return true;
+}
+// console.log(isPermutation('aba', 'baa'));
+// console.log(isPermutation('abc', 'daa'));
+// console.log(isPermutation('abc', 'da'));
+
+// #1.3
 
 
