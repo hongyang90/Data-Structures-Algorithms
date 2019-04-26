3509e5eadd6e355eb7b67cc4113da5571fccea5a
HongYang Gao <hongyang.gao@gmail.com>
2019-04-26 14:55:54 -0700

    Solve postOrderArray traversal

--- tree_order_project/lib/tree_order.js
+++ tree_order_project/lib/tree_order.js
@@ -6,11 +6,16 @@ function inOrderArray(root) {
    let left= inOrderArray(root.left);
    let right = inOrderArray(root.right);
 
-   return left.concat(val, right)
+   return left.concat(val, right);
 }
 
 function postOrderArray(root) {
+    if (!root) return [];
+    let val = root.val;
+    let left = postOrderArray(root.left);
+    let right = postOrderArray(root.right);
 
+    return left.concat(right, val);
 }
 
 
