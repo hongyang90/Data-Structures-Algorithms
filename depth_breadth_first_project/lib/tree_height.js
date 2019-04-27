0cb08ccdfa5a598b938a1420312cd91a0a6e757f
HongYang Gao <hongyang.gao@gmail.com>
2019-04-26 20:30:13 -0700

    sole treesum & tree height

--- depth_breadth_first_project/lib/tree_height.js
+++ depth_breadth_first_project/lib/tree_height.js
@@ -1,5 +1,8 @@
 function treeHeight(root) {
+    if (!root) return -1;
 
+   return  Math.max(treeHeight(root.left),treeHeight(root.max)) + 1;
+  
 }
 
 
--- depth_breadth_first_project/lib/tree_sum.js
+++ depth_breadth_first_project/lib/tree_sum.js
@@ -1,5 +1,17 @@
 function treeSum(root) {
+    if (!root) return 0;
+    return treeSum(root.left) + root.val + treeSum(root.right)
+    // let sum = 0;
+    // let queue =[root];
 
+    // while (queue.length) {
+    //     let node = queue.shift();
+    //     if (node.left) queue.push(node.left)
+    //     if (node.right) queue.push(node.right)
+    //     sum+=node.val;
+
+    // }
+    // return sum;
 }
 
 
