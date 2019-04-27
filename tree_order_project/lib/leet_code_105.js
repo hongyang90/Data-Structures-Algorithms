559d3e5dc278d275f2a6fe9dfe22c47916c275c1
HongYang Gao <hongyang.gao@gmail.com>
2019-04-26 18:25:19 -0700

    Solve leetcode #105

--- tree_order_project/lib/leet_code_105.js
+++ tree_order_project/lib/leet_code_105.js
@@ -5,5 +5,24 @@ const { TreeNode } = require('./tree_node.js');
 
 
 function buildTree(preorder, inorder) {
+    if (preorder.length === 0 && inorder.length === 0) return null;
+
+    let root = preorder[0];
+    let rootNode = new TreeNode(root);
+
+    let midIdx = inorder.indeOf(root);
+    let leftIn = inorder.slice(0,midIdx);
+    let rightIn = inorder.slice(midIdx+1);
+
+    let leftPre = preorder.filter(ele => leftIn.includes(ele));
+    let rightPre = preorder.filter(ele => rightIn.includes(ele));
+
+    let leftresult =  buildTree(leftPre, leftIn);
+    let rightresult = buildTree(rightPre, rightIn);
+
+    rootNode.left = leftresult;
+    rootNode.right = rightresult;
+
+    return rootNode;
 
 }
