f6d2992224dc7af8d4015887177c214480f024bc
HongYang Gao <hongyang.gao@gmail.com>
2019-04-29 20:08:50 -0700

    Solve problem 10.2

--- CTCI_Problems/sorts_searches.js
+++ CTCI_Problems/sorts_searches.js
@@ -20,3 +20,24 @@ function sortedMerge (arr1, arr2) {
 }
 
 console.log(sortedMerge([2,3,4,0,0], [2,6]));
+
+// Problem 10.2
+
+function groupAnagrams (array) {
+    let hash = {};
+
+    for (let i = 0; i < array.length; i++) {
+        let sorted = array[i].split('').sort().jion('');
+        if (hash[sorted] === undefined) {
+            hash[sorted] = [array[i]];
+        } else {
+            hash[sorted] = [array[i]];
+            
+        }
+    }
+    return Object.values(hash);
+}
+
+
+// Problem 10.3
+
