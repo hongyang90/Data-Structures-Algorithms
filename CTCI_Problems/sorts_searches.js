fb98f80cb846c5b6aca068eb933fade0fdd0dcef
HongYang Gao <hongyang.gao@gmail.com>
2019-04-29 20:26:02 -0700

    Solve problem 10.3

--- CTCI_Problems/sorts_searches.js
+++ CTCI_Problems/sorts_searches.js
@@ -19,7 +19,7 @@ function sortedMerge (arr1, arr2) {
     return arr1;
 }
 
-console.log(sortedMerge([2,3,4,0,0], [2,6]));
+// console.log(sortedMerge([2,3,4,0,0], [2,6]));
 
 // Problem 10.2
 
@@ -41,3 +41,36 @@ function groupAnagrams (array) {
 
 // Problem 10.3
 
+function rotatedSearch (array, target){
+    let minIdx = array.indexOf(Math.min(...array));
+    let start;
+    let end;
+    if (array[minIdx] === target) return minIdx;
+    if (target < array[0]) {
+        start = minIdx;
+        end = array.length;
+    } else {
+        start = 0;
+        if (minIdx === 0) {
+            end = array.length;
+        } else {
+            end = minIdx - 1;
+            
+        }
+    }
+
+    while (start <= end) {
+        let mid = Math.floor((start + end) / 2);
+        if (target === array[mid]) return mid;
+        if (target < array[mid]) {
+            end = mid -1;
+        } else {
+            start =mid +1;
+        }
+    }
+
+    return -1;
+
+}
+
+console.log(rotatedSearch([15,16,19,20,25,1,3,4,5,7,10,14], 5));
