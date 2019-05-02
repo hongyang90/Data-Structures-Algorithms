f0f8a73fc67eb3f3ac358573736ff7a7b58d98db
HongYang Gao <hongyang.gao@gmail.com>
2019-05-01 19:00:15 -0700

    Solve leetcode 110

--- bst_project/lib/leet_code_110.js
+++ bst_project/lib/leet_code_110.js
@@ -3,5 +3,19 @@
 
 
 function isBalanced(root) {
+    if (!root) {
+        return true;
+    }
 
+    let diff = Math.abs(treeHeight(root.left), treeHeight(root.right)) <= 1;
+
+
+    return diff && isBalanced(right) && isBalanced(left);
+
+}
+
+function treeHeight(root) {
+    if (!root) return -1;
+
+    return Math.max(treeHeight(root.left)+1, treeHeight(root.right) + 1);
 }
