f5e8ae37b5f3bfa703ddfbccc5e8314ceeef7ff2
HongYang Gao <hongyang.gao@gmail.com>
2019-05-01 20:02:54 -0700

    SOlve leetcode 108

--- bst_project/lib/leet_code_108.js
+++ bst_project/lib/leet_code_108.js
@@ -3,5 +3,15 @@
 
 
 function sortedArrayToBST(nums) {
+    if (nums.length === 0) return null;
 
+    let mid = Math.floor(nums.length/2);
+    let root = new TreeNode(nums[mid]);
+
+    let lefttree = sortedArrayToBST(nums.slice(0,mid));
+    let righttree = sortedArrayToBST(nums.slice(mid+1));
+
+    root.left = lefttree;
+    root.right = righttree;
+    return root;
 }
