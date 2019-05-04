5690cfa7a2b9edafdb43549e1df6f4fd07d7edb4
HongYang Gao <hongyang.gao@gmail.com>
2019-05-03 17:47:37 -0700

    Solve max value of graph

--- graph_project/lib/max_value.js
+++ graph_project/lib/max_value.js
@@ -1,5 +1,19 @@
 function maxValue(node, visited=new Set()) {
-
+    let queue = [node];
+    let max = node.val;
+    while (queue.length) {
+        let curr = queue.shift();
+        if (curr.val > max ) {
+            max = curr.val;
+        }
+        visited.add(curr);
+        curr.neighbors.forEach( node => {
+            if (!visited.has(node)) {
+                queue.push(node);
+            }
+        });
+    }
+    return max;
 }
 
 module.exports = {
