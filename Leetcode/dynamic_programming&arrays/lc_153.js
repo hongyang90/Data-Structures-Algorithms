c69fee850ebcf19050525e76e40c7680bbf16269
HongYang Gao <hongyang.gao@gmail.com>
2019-06-14 14:47:16 -0700

    Fix leetcode #153

--- Leetcode/dynamic_programming&arrays/lc_153.js
+++ Leetcode/dynamic_programming&arrays/lc_153.js
@@ -2,6 +2,7 @@ function minInRotatedArray (arr) {
     let start = 0;
     let end = arr.length;
     while (start < end){
+        if (arr[start]<arr[end]) return arr[start];
         let mid = Math.floor((start+end)/2);
         if (arr[mid] > arr[end]){
             start = mid +1;
