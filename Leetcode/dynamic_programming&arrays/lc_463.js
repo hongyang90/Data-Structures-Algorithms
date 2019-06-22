d5ffb4899f7ad215252b698c9323b11769a59c42
HongYang Gao <hongyang.gao@gmail.com>
2019-06-21 23:14:07 -0700

    Solve leetcode #463

---
+++ Leetcode/dynamic_programming&arrays/lc_463.js
@@ -0,0 +1,15 @@
+function islandPerimeter(grid){
+    let count = 0;
+    for (let i=0; i< grid.length; i++){
+        for (let j=0; j< grid[i].length; j++){
+            if (grid[i][j] === 1){
+                if (!grid[i-1] || grid[i-1][j] === 0) count++;
+                if (!grid[i+1] || grid[i+1][j] === 0) count++;
+                if (!grid[i][j-1] || grid[i][j-1] === 0) count++;
+                if (!grid[i][j+1] || grid[i][j+1] === 0) count++;
+            }
+        }
+    }
+
+    return count;
+}
