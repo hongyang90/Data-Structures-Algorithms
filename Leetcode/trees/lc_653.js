74555c27c946a187167329bbd317d5044496fe88
HongYang Gao <hongyang.gao@gmail.com>
2019-06-28 16:10:29 -0700

    Update leetcode #653

--- Leetcode/trees/lc_653.js
+++ Leetcode/trees/lc_653.js
@@ -1,10 +1,18 @@
-function twoSum (root, target){
+function twoSum (root, target, set = new Set()){
     if (!root) return false;
-    if (root.val === target) {
-        return true;
-    } else if (root.val > target){
-        return twoSum(root.left, target);
-    } else {
-        return twoSum(root.right, target)
+
+    let queue =[root];
+    while (queue.length){
+        let val = queue.shift().val
+        let curr = target - val;
+        if (set.has(curr)){
+            return true;
+        } else {
+            set.add(val);
+        }
+        if (!root.left) queue.push(root.left);
+        if (!root.right) queue.push(root.right);
+
     }
+    return false;
 }
